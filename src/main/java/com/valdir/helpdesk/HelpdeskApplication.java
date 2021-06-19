4711b7e3efc4043395cf909ed67572b875d82dbe
ValdirCezar <valdircezar312@gmail.com>
2021-06-19 11:21:11 -0300

    projeto iniciado

---
+++ src/main/java/com/valdir/helpdesk/HelpdeskApplication.java
@@ -0,0 +1,13 @@
+package com.valdir.helpdesk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelpdeskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelpdeskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/valdir/helpdesk/HelpdeskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.valdir.helpdesk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelpdeskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
