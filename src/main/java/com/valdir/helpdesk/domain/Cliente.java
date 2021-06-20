5aa2d1f74b7312b3f0f2d5faa5d5a336f4894e50
ValdirCezar <valdircezar312@gmail.com>
2021-06-20 15:50:05 -0300

    protegendo classes contra serialização

--- src/main/java/com/valdir/helpdesk/domain/Cliente.java
+++ src/main/java/com/valdir/helpdesk/domain/Cliente.java
@@ -6,12 +6,14 @@ import java.util.List;
 import javax.persistence.Entity;
 import javax.persistence.OneToMany;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.valdir.helpdesk.domain.enums.Perfil;
 
 @Entity
 public class Cliente extends Pessoa {
 	private static final long serialVersionUID = 1L;
 	
+	@JsonIgnore
 	@OneToMany(mappedBy = "cliente")
 	private List<Chamado> chamados = new ArrayList<>();
 
