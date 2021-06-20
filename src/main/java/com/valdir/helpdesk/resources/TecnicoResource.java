5d71b4a649d0b1a67c959633bb22e9c349c53427
ValdirCezar <valdircezar312@gmail.com>
2021-06-20 15:50:23 -0300

    endpoint findById para Tecnico

---
+++ src/main/java/com/valdir/helpdesk/resources/TecnicoResource.java
@@ -0,0 +1,26 @@
+package com.valdir.helpdesk.resources;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.valdir.helpdesk.domain.Tecnico;
+import com.valdir.helpdesk.services.TecnicoService;
+
+@RestController
+@RequestMapping(value = "/tecnicos")
+public class TecnicoResource {
+	
+	@Autowired
+	private TecnicoService service;
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Tecnico> findById(@PathVariable Integer id) {
+		Tecnico obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
+	}
+
+}
---
+++ src/main/java/com/valdir/helpdesk/services/TecnicoService.java
@@ -0,0 +1,21 @@
+package com.valdir.helpdesk.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.valdir.helpdesk.domain.Tecnico;
+import com.valdir.helpdesk.repositories.TecnicoRepository;
+
+@Service
+public class TecnicoService {
+
+	@Autowired
+	private TecnicoRepository repository;
+	
+	public Tecnico findById(Integer id) {
+		Optional<Tecnico> obj = repository.findById(id);
+		return obj.orElse(null);
+	}
+}
