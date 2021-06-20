e2882f080f5e6b1e20619da35a97a41a2cd5c482
ValdirCezar <valdircezar312@gmail.com>
2021-06-20 17:56:26 -0300

    endpoint findAll para Tecnico

--- src/main/java/com/valdir/helpdesk/resources/TecnicoResource.java
+++ src/main/java/com/valdir/helpdesk/resources/TecnicoResource.java
@@ -1,5 +1,8 @@
 package com.valdir.helpdesk.resources;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,14 +17,21 @@ import com.valdir.helpdesk.services.TecnicoService;
 @RestController
 @RequestMapping(value = "/tecnicos")
 public class TecnicoResource {
-	
+
 	@Autowired
 	private TecnicoService service;
-	
+
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<TecnicoDTO> findById(@PathVariable Integer id) {
 		Tecnico obj = service.findById(id);
 		return ResponseEntity.ok().body(new TecnicoDTO(obj));
+	}
+	
+	@GetMapping
+	public ResponseEntity<List<TecnicoDTO>> findAll() {
+		List<Tecnico> list = service.findAll();
+		List<TecnicoDTO> listDTO = list.stream().map(obj -> new TecnicoDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDTO);
 	}
 
 }
--- src/main/java/com/valdir/helpdesk/services/TecnicoService.java
+++ src/main/java/com/valdir/helpdesk/services/TecnicoService.java
@@ -1,5 +1,6 @@
 package com.valdir.helpdesk.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,4 +20,9 @@ public class TecnicoService {
 		Optional<Tecnico> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ObjectnotFoundException("Objeto não encontrado! Id: " + id));
 	}
+
+	public List<Tecnico> findAll() {
+		return repository.findAll();
+	}
+
 }
