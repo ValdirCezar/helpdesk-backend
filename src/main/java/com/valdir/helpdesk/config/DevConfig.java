f622e4ddd921604918eed2e807f0f42144583f12
ValdirCezar <valdircezar312@gmail.com>
2021-06-20 00:53:00 -0300

    perfil de desenvolvimento

---
+++ src/main/java/com/valdir/helpdesk/config/DevConfig.java
@@ -0,0 +1,28 @@
+package com.valdir.helpdesk.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.valdir.helpdesk.services.DBService;
+
+@Configuration
+@Profile("dev")
+public class DevConfig {
+
+	@Autowired
+	private DBService dbService;
+	
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String value;
+	
+	@Bean
+	public boolean instanciaDB() {
+		if(value.equals("create")) {
+			this.dbService.instanciaDB();
+		}
+		return false;
+	}
+}
