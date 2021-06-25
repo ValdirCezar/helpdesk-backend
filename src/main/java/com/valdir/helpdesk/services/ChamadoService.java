7506e212cf4163b31658b0e1c49a48baa2a60d76
ValdirCezar <valdircezar312@gmail.com>
2021-06-25 13:08:36 -0300

    update para Chamado implementado

--- src/main/java/com/valdir/helpdesk/services/ChamadoService.java
+++ src/main/java/com/valdir/helpdesk/services/ChamadoService.java
@@ -1,7 +1,10 @@
 package com.valdir.helpdesk.services;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
+
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -38,6 +41,13 @@ public class ChamadoService {
 		return repository.save(newChamado(obj));
 	}
 
+	public Chamado update(Integer id, @Valid ChamadoDTO objDTO) {
+		objDTO.setId(id);
+		Chamado oldObj = findById(id);
+		oldObj = newChamado(objDTO);
+		return repository.save(oldObj);
+	}
+
 	private Chamado newChamado(ChamadoDTO obj) {
 		Tecnico tecnico = tecnicoService.findById(obj.getTecnico());
 		Cliente cliente = clienteService.findById(obj.getCliente());
@@ -45,6 +55,10 @@ public class ChamadoService {
 		Chamado chamado = new Chamado();
 		if(obj.getId() != null) {
 			chamado.setId(obj.getId());
+		}
+		
+		if(obj.getStatus().equals(2)) {
+			chamado.setDataFechamento(LocalDate.now());
 		}
 		
 		chamado.setTecnico(tecnico);
