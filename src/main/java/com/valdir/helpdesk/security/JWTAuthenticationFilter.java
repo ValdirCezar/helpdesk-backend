274a562cc97c9109deffee6535b6e83059c488d9
Valdir Cezar <58149445+ValdirCezar@users.noreply.github.com>
2023-04-19 09:00:55 -0300

    Update JWTAuthenticationFilter.java

--- src/main/java/com/valdir/helpdesk/security/JWTAuthenticationFilter.java
+++ src/main/java/com/valdir/helpdesk/security/JWTAuthenticationFilter.java
@@ -51,8 +51,10 @@ public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilte
 	
 		String username = ((UserSS) auth.getPrincipal()).getUsername();
 		String token = jwtUtil.generateToken(username);
-		res.addHeader("Authorization", "Bearer " + token);
-		res.addHeader("access-control-expose-headers", "Authorization");
+		response.setHeader("Access-Control-Allow-Origin", "*");
+        	response.setHeader("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS, DELETE");
+        	response.setHeader("Access-Control-Allow-Headers", "Authorization, Content-Type, enctype, Location");
+        	response.setHeader("Authorization", "Bearer " + token);
 	}
 	
 	@Override
